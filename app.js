6f09c4bc86f040240734d8e8f91bdc5081a9408e
JsonClaros <jayson.claros@gmail.com>
2026-02-05 11:43:34 +0800

    update

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
+// app.js
 const express = require('express');
 const app = express();
 const path = require('path');
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.render('index');  
-});
+app.get('/', (req, res) => res.render('index'));
+app.get('/projects', (req, res) => res.render('projects'));
+app.get('/contact', (req, res) => res.render('contact'));
 
-app.get('/projects', (req, res) => {
-  res.render('projects');
-});
-
-app.get('/contact', (req, res) => {
-  res.render('contact');
-});
-
-app.listen(3000, () => {
-  console.log('Portfolio running at http://localhost:3000');
-});
+module.exports = app;
